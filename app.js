68210529581f7d28d7c3544c7f2af3156dd668f9
sir.gippo <ibodrov3@gmail.com>
2024-05-16 14:19:10 +0200

    nothing

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const db = require('./services/database.js');
+
 const path = require('path');
 const ejs =require('ejs');
 
